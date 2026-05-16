58ba0408633afe49d37aa79fcc625537617d2405
Akshat Srivastava <akshatsrivastavavk@gmail.com>
2026-05-16 13:24:30 +0530

    Add Register Validations

---
+++ src/middlewares/validator.middleware.js
@@ -0,0 +1,11 @@
+import { validationResult } from "express-validator";
+import { ApiError } from "../utils/api-error.js";
+export const validate=(req,res,next)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty()){
+        const extractedErrors=[];
+        errors.array().map(err=>extractedErrors.push({[err.path]:err.msg}));   
+        throw new ApiError(422,"Validation Error/Recieved Data is invalid",extractedErrors);
+    }
+    next();
+}
